bc89c90b8409cbc8c9958692389b2c561f310eea
Eve <corneievacristina2006@gmail.com>
2026-08-30 04:40:37 +0300

    improved visuals and added pulse function

--- experiments/first_neuron.py
+++ experiments/first_neuron.py
@@ -2,41 +2,69 @@ from neurosim.neuron import LIFneuron
 import matplotlib.pyplot as plt
 
 
+
+def add_pulse(stimulation, start_time, end_time, amplitude):
+    for t in range(start_time, end_time):
+        stimulation[t] = amplitude
+
+
+
+
+stimulation_length = 500  # milliseconds
+
 times = []
 voltages = []
 spike_times = []
 
+stimulation = [0.0] * stimulation_length
+
+
+add_pulse(stimulation, 97, 103, 80.0)
+add_pulse(stimulation, 245, 250, 90.0)
+add_pulse(stimulation, 398, 400, 100.0)
+
+
 neuron = LIFneuron()
 
-for t in range(500):
-    spiked = neuron.step(input_current=17.0)
+for t in range(stimulation_length):
+    spiked = neuron.step(input_current=stimulation[t])
 
     times.append(t)
-    voltages.append(neuron.voltage)
 
     if spiked:
         spike_times.append(t)
+        voltages.append(neuron.v_threshold)
+    else:
+        voltages.append(neuron.voltage)
 
 print(f"Total spikes: {len(spike_times)}")
 print(f"Spike times: {spike_times}")
 
-plt.plot(times, voltages)
 
-plt.xlabel("Time (ms)")
-plt.ylabel("Membrane potential (mV)")
-plt.title("LIF Neuron")
+fig, (ax1, ax2) = plt.subplots(
+    2,
+    1,
+    sharex=True
+)
 
-plt.axhline(
-    y=neuron.v_threshold,
+
+ax1.plot(times, stimulation)
+
+ax1.set_ylabel("Input")
+ax1.set_title("Stimulation")
+
+
+ax2.plot(times, voltages)
+
+ax2.axhline(
+    neuron.v_threshold,
     linestyle="--",
     label="Spike threshold"
 )
 
-for spike_time in spike_times:
-    plt.axvline(
-        x=spike_time,
-        alpha=0.25
-    )
+ax2.set_xlabel("Time (ms)")
+ax2.set_ylabel("Membrane potential (mV)")
+ax2.legend()
 
-plt.legend()
+ax2.legend()
 plt.show()
