23ab1d3141bb723ad45eeca45e7a44d6ca7c7975
Eve <corneievacristina2006@gmail.com>
2026-08-31 23:08:05 +0300

    Made simulation and visualization modular

--- experiments/first_neuron.py
+++ experiments/first_neuron.py
@@ -27,13 +27,38 @@ def score_spikes(target_spikes, actual_spikes, tolerance=10):
     return 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0.0
 
 
+def visualize_spike_times(spike_times, target_spikes, neuron):
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    ax1.step(times, stimulation, where="post")
+
+    ax1.set_ylabel("Input")
+    ax1.set_title("lif neuron simulation")
+
+    ax2.plot(times, voltages)
+
+    ax2.axhline(neuron.v_threshold, linestyle="--", label="Spike threshold")
+
+    for spike_time in target_spikes:
+        ax2.axvline(spike_time, linestyle=":", color="red", linewidth=4.5,)
+
+    for spike_time in spike_times:
+        ax2.vlines(spike_time, neuron.v_reset, neuron.v_threshold, alpha=0.5)
+
+    ax2.set_xlabel("Time (ms)")
+    ax2.set_ylabel("Membrane potential (mV)")
+    ax2.legend()
+
+    ax2.legend()
+    plt.show()
+
+
 stimulation_length = 500  # milliseconds
 tolerance = 10
 
 
 times = []
 voltages = []
-spike_times = []
+
 target_spikes = [100, 200, 400]
 
 
@@ -43,62 +68,35 @@ stimulation = [0.0] * stimulation_length
 
 add_pulse(stimulation, 98, 102, 60.0)
 add_pulse(stimulation, 198, 202, 60.0)
-#add_pulse(stimulation, 397, 402, 60.0)
+add_pulse(stimulation, 397, 402, 60.0)
 
 
-neuron = LIFneuron()
 
-for t in range(stimulation_length):
-    spiked = neuron.step(input_current=stimulation[t])
+def run_simulation(stimulation):
+    neuron = LIFneuron()
 
-    times.append(t)
+    spike_times = []
 
-    if spiked:
-        spike_times.append(t)
-        voltages.append(neuron.v_threshold)
-    else:
-        voltages.append(neuron.voltage)
+    for t in range(stimulation_length):
+        spiked = neuron.step(input_current=stimulation[t])
 
-print(f"Total spikes: {len(spike_times)}")
-print(f"Spike times: {spike_times}")
-print(f"Score: {score_spikes(target_spikes, spike_times, tolerance)}")
+        times.append(t)
 
-fig, (ax1, ax2) = plt.subplots(
-    2,
-    1,
-    sharex=True
-)
+        if spiked:
+            spike_times.append(t)
+            voltages.append(neuron.v_threshold)
+        else:
+            voltages.append(neuron.voltage)
 
 
-ax1.step(times, stimulation, where="post")
-
-ax1.set_ylabel("Input")
-ax1.set_title("lif neuron simulation")
-
-
-ax2.plot(times, voltages)
-
-ax2.axhline(
-    neuron.v_threshold,
-    linestyle="--",
-    label="Spike threshold"
-)
-
-for spike_time in target_spikes:
-    ax2.axvline(
-        spike_time,
-        linestyle=":",
-        color="red",
-        linewidth=4.5,
-    )
+    print(f"Total spikes: {len(spike_times)}")
+    print(f"Spike times: {spike_times}")
+    print(f"Score: {score_spikes(target_spikes, spike_times, tolerance)}")
 
 
-for spike_time in spike_times:
-    ax2.vlines(spike_time, neuron.v_reset, neuron.v_threshold, alpha=0.5)
+    visualize_spike_times(spike_times, target_spikes, neuron)
+    return spike_times
 
-ax2.set_xlabel("Time (ms)")
-ax2.set_ylabel("Membrane potential (mV)")
-ax2.legend()
 
-ax2.legend()
-plt.show()
+spike_times = run_simulation(stimulation)
+
