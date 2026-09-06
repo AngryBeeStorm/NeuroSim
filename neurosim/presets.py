c93b6acca58bc5f66762a8acda32ff3087cc1891
Eve <corneievacristina2006@gmail.com>
2026-09-06 17:38:18 +0300

    Integrating presets for spike activity

---
+++ neurosim/presets.py
@@ -0,0 +1,23 @@
+
+
+PRESETS = {
+    "Regular": {
+        "spikes": [100, 200, 300, 400],
+        "description": "Evenly spaced firing."
+    },
+
+    "Burst": {
+        "spikes": [200, 210, 220],
+        "description": "A cluster of rapid spikes."
+    },
+
+    "Double burst": {
+        "spikes": [100, 110, 120, 350, 360, 370],
+        "description": "Two separate bursts of activity."
+    },
+
+    "Sparse": {
+        "spikes": [100, 400],
+        "description": "Two widely separated spikes."
+    }
+}
